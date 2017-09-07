0f5fc111096130a88a6a089f0bd7e083a87d7886
Carlos Duran <carlosduran.xyz@gmail.com>
2017-09-07 14:21:23 -0500

    Disable scrolling prevent

--- slideshow.js
+++ slideshow.js
@@ -191,12 +191,12 @@
       startX = touchobj.pageX
       startY = touchobj.pageY
       startTime = new Date().getTime() // record time when finger first makes contact with surface
-      e.preventDefault()
+      //e.preventDefault()
     }, false)
 
-    touchsurface.addEventListener('touchmove', function(e){
-      e.preventDefault() // prevent scrolling when inside DIV
-    }, false)
+    // touchsurface.addEventListener('touchmove', function(e){
+    //   e.preventDefault() // prevent scrolling when inside DIV
+    // }, false)
 
     touchsurface.addEventListener('touchend', function(e){
       var touchobj = e.changedTouches[0]
@@ -212,7 +212,7 @@
         }
       }
       handleswipe(swipedir)
-      e.preventDefault()
+      //e.preventDefault()
     }, false)
   }
 
